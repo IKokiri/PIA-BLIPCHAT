5f5715398050dd2a950d016c059d2d23413146f8
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 19:20:09 -0300

    adiçao de link no menu

--- src/components/MenuDown.js
+++ src/components/MenuDown.js
@@ -10,13 +10,19 @@ export const MenuDown = () =>{
         <div className="fixed-bottom">            
             <ul class="nav justify-content-center">
                 <li class="nav-item">
-                    <img src={home} alt="Tela incial" />
+                    <a class="nav-link active" href="https://google.com.br">    
+                        <img src={home} alt="Tela incial" />
+                    </a>                    
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link active" href="https://google.com.br">
                     <img src={copy} alt="Copiar" />
+                    </a>                    
                 </li>
                 <li class="nav-item">
-                <img src={chat} alt="Chat" />
+                    <a class="nav-link active" href="https://google.com.br">
+                        <img src={chat} alt="Chat" />
+                    </a>
                 </li>
             </ul>        
         </div>

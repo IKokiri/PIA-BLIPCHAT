987ee37f7549f3d2b3f96e7e2717a7901e369144
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 18:15:23 -0300

    adiçao de label

--- src/components/AppKey.js
+++ src/components/AppKey.js
@@ -5,9 +5,10 @@ export const AppKey = () =>{
     return(
         <>
             <div class="form-group">
+                <label>KEY</label>
                 <input type="text" class="form-control" id="appKey" aria-describedby="appKeyHelp" placeholder="APP KEY"/>
                 <p id="appKeyHelp">
-                    <label>Inserir key do seu chat boot.</label>
+                    <small>Inserir key do seu chat boot.</small>
                 </p>
             </div>
         </>

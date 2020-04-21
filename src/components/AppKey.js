ea4a205ba2836e6baf328ca075b2977b941045e0
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 18:20:11 -0300

    adiçao de for para smal

--- src/components/AppKey.js
+++ src/components/AppKey.js
@@ -5,10 +5,9 @@ export const AppKey = () =>{
     return(
         <>
             <div class="form-group">
-                <label>KEY</label>
                 <input type="text" class="form-control" id="appKey" aria-describedby="appKeyHelp" placeholder="APP KEY"/>
                 <p id="appKeyHelp">
-                    <small>Inserir key do seu chat boot.</small>
+                    <small for="appKey">Inserir key do seu chat boot.</small>
                 </p>
             </div>
         </>

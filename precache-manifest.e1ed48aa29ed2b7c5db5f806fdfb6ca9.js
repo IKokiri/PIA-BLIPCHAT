6024c78850e85fb87b2dc3c7af1a02764eceb023
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 13:19:54 -0300

    Updates

---
+++ precache-manifest.e1ed48aa29ed2b7c5db5f806fdfb6ca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d2b87fd0c8a7c743adb1620f4f1a0a",
+    "url": "/pia-blipchat/index.html"
+  },
+  {
+    "revision": "a516d481825c87f87aef",
+    "url": "/pia-blipchat/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3134c46a9e7db7c3dd69",
+    "url": "/pia-blipchat/static/js/2.55afe5ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pia-blipchat/static/js/2.55afe5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a516d481825c87f87aef",
+    "url": "/pia-blipchat/static/js/main.06ce29cd.chunk.js"
+  },
+  {
+    "revision": "e297fb5a847a270e6b92",
+    "url": "/pia-blipchat/static/js/runtime-main.7623c6ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pia-blipchat/static/media/logo.5d5d9eef.svg"
+  }
+]);

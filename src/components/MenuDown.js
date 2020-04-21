07dc45ebfddd9aa03d8da007f2f5fcb8518ce927
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 18:36:59 -0300

    correçao posicionamento menu

--- src/components/MenuDown.js
+++ src/components/MenuDown.js
@@ -3,13 +3,18 @@ import React from 'react'
 export const MenuDown = () =>{
     
     return(
-        <div>
-            MenuDown
-            <br />
-            Copiar
-            <br />
-            Chat
-            
+        <div className="fixed-bottom">            
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Copiar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Chat</a>
+                </li>
+            </ul>        
         </div>
     )
 }

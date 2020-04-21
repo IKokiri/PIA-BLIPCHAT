327faa13faf95e3fcce452cf8679738b67ab8faf
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 17:48:10 -0300

    configuraçao dos modulos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppKey } from './components'
 import { Code } from './components'
 import { MenuDown } from './components'

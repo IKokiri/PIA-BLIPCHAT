f77e49ffdb61f404f6a32a3e2e5bae4fe91c2527
IKokiri <developer.luizmendes@gmail.com>
2020-04-21 17:57:43 -0300

    correçao sintaxe react

--- src/components/AppKey.js
+++ src/components/AppKey.js
@@ -3,8 +3,10 @@ import React from 'react'
 export const AppKey = () =>{
     
     return(
-        <div class="form-group">
-            <input type="text" class="form-control" id="appKey" aria-describedby="appKeyHelp">
-        </div>
+        <>
+            <div class="form-group">
+                <input type="text" class="form-control" id="appKey" aria-describedby="appKeyHelp" />
+            </div>
+        </>
     )
 }

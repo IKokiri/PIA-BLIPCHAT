bf49e66668e49106d6400d0dc57f11618a0cd301
IKokiri <developer.luizmendes@gmail.com>
2020-04-27 22:00:38 +0000

    Updates

--- precache-manifest.3efef60652f3fb4ecdc13780561f2383.js
+++ precache-manifest.3efef60652f3fb4ecdc13780561f2383.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "984e2ea69c1f58a7ffb6a773dd275373",
+    "revision": "3c8952eb31ba6f1d50ea644a4f88f45e",
     "url": "/pia-blipchat/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pia-blipchat/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "8bcf14a4ea1286a0d643",
+    "revision": "6146de18261736f9e4db",
     "url": "/pia-blipchat/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pia-blipchat/static/js/2.a8109f56.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8bcf14a4ea1286a0d643",
-    "url": "/pia-blipchat/static/js/main.a1239347.chunk.js"
+    "revision": "6146de18261736f9e4db",
+    "url": "/pia-blipchat/static/js/main.4ea19212.chunk.js"
   },
   {
     "revision": "e297fb5a847a270e6b92",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pia-blipchat/precache-manifest.b4c1798b054162a2d7efbfd5bae6aaa5.js"
+  "/pia-blipchat/precache-manifest.3efef60652f3fb4ecdc13780561f2383.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pia-blipchat/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

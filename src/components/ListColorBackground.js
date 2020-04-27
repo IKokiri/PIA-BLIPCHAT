abe17e0b4c217bcec60394d6c139d777d312fe73
IKokiri <developer.luizmendes@gmail.com>
2020-04-27 18:07:57 -0300

    adilao de carregamento de cores

--- src/components/ListColorBackground.js
+++ src/components/ListColorBackground.js
@@ -1,29 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import {Colors} from "../components"
+import { Color  } from '../api/Color';
 
 export const ListColorBackground = () =>{
+    const [colors, setColors] = useState([]);
+
+    function startUp(){
+        
+        let c = Color.getColors();
+        
+        c.then(
+            function(data){
+                setColors(data.schemes[0].colors);
+                
+            }
+        )
+    }
+    useEffect(() => startUp(),[])
     
     return(
         <>
             <h3>Background</h3>
             <div className="row colors container">
             </div>
-           {loadColors()}
+           
+           {colors.map(color=>
+               <Colors color={color} key={color} local="background"></Colors> 
+            )}
         </>
     )
-
-    function loadColors(){
-       let colors = "";
-
-        fetch('https://gist.githubusercontent.com/kawanet/a880c83f06d6baf742e45ac9ac52af96/raw/b4fbc9a730394eb977277e73cc37b60955463f21/material-colors.json', {
-            method: 'GET',
-            }).then(res=>res.json())
-            .then(res =>{
-                for ( let i in res){
-                    
-                    colors += '<div class="col-3"><button type="button" style="background-color: '+res[i]['900']+'" class="btn">&nbsp;&nbsp;&nbsp;&nbsp;</button></div>'
-                    
-                }
-                document.querySelector(".colors").innerHTML = colors;
-            });
-    }
 }

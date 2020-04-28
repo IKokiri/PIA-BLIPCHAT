7f90631727b6fdd52a5468bc541224aa0a0556c1
IKokiri <developer.luizmendes@gmail.com>
2020-04-28 14:11:16 -0300

    correçao rota

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { ListColorBackground } from './components';
 import { ListColorSend } from './components';
 import { ListColorReicived } from './components';
 import { ChatIcon } from './components';
-import {  BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {  BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   

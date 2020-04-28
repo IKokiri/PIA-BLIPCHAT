4cb15d5d7b0d6b5bedba7bb434e74d74020c5814
IKokiri <developer.luizmendes@gmail.com>
2020-04-28 18:25:12 +0000

    Updates

--- precache-manifest.6bff52b5ce809d92091006858a1b7d09.js
+++ precache-manifest.6bff52b5ce809d92091006858a1b7d09.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d9596e84d131c3045ad0b66c5400e0a",
+    "revision": "c72166c4f7911eec6e99f3e56afb5f9a",
     "url": "/pia-blipchat/index.html"
   },
   {
-    "revision": "79c50dae43953c898556",
+    "revision": "9abb71bbfe50e3f13497",
     "url": "/pia-blipchat/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "f0dccc9ef3a78edd76ec",
+    "revision": "32aafa8532c012f31688",
     "url": "/pia-blipchat/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "79c50dae43953c898556",
-    "url": "/pia-blipchat/static/js/2.a8109f56.chunk.js"
+    "revision": "9abb71bbfe50e3f13497",
+    "url": "/pia-blipchat/static/js/2.6f53f5c1.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/pia-blipchat/static/js/2.a8109f56.chunk.js.LICENSE.txt"
+    "url": "/pia-blipchat/static/js/2.6f53f5c1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f0dccc9ef3a78edd76ec",
-    "url": "/pia-blipchat/static/js/main.28c786e7.chunk.js"
+    "revision": "32aafa8532c012f31688",
+    "url": "/pia-blipchat/static/js/main.2a2556ca.chunk.js"
   },
   {
     "revision": "e297fb5a847a270e6b92",

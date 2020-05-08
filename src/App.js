502cddd12dd4a8e5ee0325c66256bfc97037fbc8
IKokiri <39102835+IKokiri@users.noreply.github.com>
2020-05-08 09:35:01 -0300

    Teste
    teste

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Blip from './components/Blip'
-import { AppKey } from './components';
-import { Code } from './components';
-import { MenuDown } from './components';
-import { MenuUp } from './components';
-import { ListColorHeader } from './components';
-import { ListColorBackground } from './components';
-import { ListColorSend } from './components';
-import { ListColorReicived } from './components';
-import { ChatIcon } from './components';
-import {  BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Blip from "./components/Blip";
+import { AppKey } from "./components";
+import { Code } from "./components";
+import { MenuDown } from "./components";
+import { MenuUp } from "./components";
+import { ListColorHeader } from "./components";
+import { ListColorBackground } from "./components";
+import { ListColorSend } from "./components";
+import { ListColorReicived } from "./components";
+import { ChatIcon } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  
   return (
     <div className="App">
-      
-      <div className="container">  
-      
-      <AppKey></AppKey> 
-      <Router>
-          <Switch>  
+      <div className="container">
+        <AppKey />
+        <Router>
+          <Switch>
             <Route path="/ChatIcon" exact component={ChatIcon} />
             <Route path="/ListColorSend" exact component={ListColorSend} />
-            <Route path="/ListColorReicived" exact component={ListColorReicived} />
-            <Route path="/ListColorBackground" exact component={ListColorBackground} />
-            <Route path="/ListColorHeader" exact component={ListColorHeader} />         
-            
+            <Route
+              path="/ListColorReicived"
+              exact
+              component={ListColorReicived}
+            />
+            <Route
+              path="/ListColorBackground"
+              exact
+              component={ListColorBackground}
+            />
+            <Route path="/ListColorHeader" exact component={ListColorHeader} />
           </Switch>
 
-          <MenuUp></MenuUp>  
-        <MenuDown></MenuDown> 
-        </Router>   
-      <Blip></Blip>   
-        <Code></Code>
-      
+          <MenuUp />
+          <MenuDown />
+        </Router>
+        <Blip />
+        <Code />
       </div>
     </div>
   );

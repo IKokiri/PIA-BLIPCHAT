6bc87de0b340a4d818601f7fe6d757fe25068e76
IKokiri <39102835+IKokiri@users.noreply.github.com>
2020-05-19 19:29:37 -0300

    Teste para entrega
    Teste para entrega do exercicio 10

--- src/components/AppKey.js
+++ src/components/AppKey.js
@@ -7,7 +7,7 @@ export const AppKey = () =>{
             <div className="form-group">
                 <input type="text" onChange={()=>AppKeychange()} className="form-control" id="appKey" aria-describedby="appKeyHelp" placeholder="APP KEY"/>
                 <p id="appKeyHelp">
-                    <small>Inserir key do seu chat boot.</small>
+                    <small>Inserir App Key do seu chat boot.</small>
                 </p>
             </div>
         </>
